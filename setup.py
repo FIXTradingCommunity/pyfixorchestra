aa1ea3f7b84b9dae6e86bde9dfaa4e059eb5ad82
Nat Judge <50224920+NattyJ798@users.noreply.github.com>
2020-07-21 16:28:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 import setuptools
 
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyfixorchestra",
+    name="pyfixorchestra-NattyJ798", # Replace with your own username
     version="1.0.0",
     author="Nathanael Judge",
     author_email="nzjudge@mtu.edu",
-    description="A python package to create a  python dictionary of FIX latest",
+    description="FIX pyorchestra",
     long_description=long_description,
-    url="https://github.com/FIXTradingCommunity/pyfixorchestra,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FIXTradingCommunity/pyfixorchestra",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
